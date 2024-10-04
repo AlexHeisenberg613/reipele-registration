c2bd9bcd02cff643c3f0dbc62d3b14535f79b693
Alex Heisenberg <alexjoseferreiranascimento@gmail.com>
2024-10-04 10:29:55 -0300

    feat: apenas criei um botão com hover para ter uma breve interatividade com o usuário pra quando o usuário passar o cursor por cima para demonstração ao requisitante do projeto. O projeto vai estar em stand by, esperando atualizações de layout que ainda não foi definido.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center p-2">
       <h1 className='text-3xl font-bold text-blue-700 underline'>
-        Hello world!!!
+        Atletas do Rei Pelé
       </h1>
+      <Link
+        className="mt-4 border border-zinc-400 p-1 rounded bg-green-500 hover:bg-green-200 hover:border-2 duration-700 font-medium"
+        to='/formulariocadastral'
+      >
+        Página de cadastro
+      </Link>
     </div>
   )
 }
