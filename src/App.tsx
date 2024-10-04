b9f7dc073137230c5f43e235d3014dfa422242bb
Alex Heisenberg <alexjoseferreiranascimento@gmail.com>
2024-10-04 16:02:44 -0300

    feat: adicionei mais arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
+import RegistrationPage from "./pages/registrationPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/cadastroatleta",
+    element: <RegistrationPage />
   }
 ])
 
---
+++ src/pages/registrationPage/index.tsx
@@ -0,0 +1,7 @@
+export default function RegistrationPage() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold">Página do Formulário de Requisição de Regisro de Atleta</h1>
+    </div>
+  )
+}
